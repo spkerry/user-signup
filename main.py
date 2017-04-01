2477b4e88587a45daff3752ab6434582c3f69258
Sean Kerry <spkerry@live.com>
2017-03-31 19:21:38 -0500

    Added get and post functions to Mainhandler; add validation functions with regular expressions

--- main.py
+++ main.py
@@ -17,6 +17,9 @@
 #
 import webapp2
 import cgi
+import re
+import os
+import string
 
 page_header = """
 <!DOCTYPE html>
@@ -38,65 +41,142 @@ page_footer = """
 </html>
 """
 
+username =""
+signup_form = """
+    <form  method="post">
+        <header>
+            <h1>Signup</h1>
+        </header>
+        <table>
+            <tbody>
+                <tr>
+                    <td class="label">
+                        Username
+                    </td>
+                    <td>
+                        <input name="username" type="text" value="%(username)s">
+
+                    </td>
+                    <td>
+                        <div class="error">%(error_username)s</div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="password">Password</label>
+                    </td>
+                    <td>
+                        <input name="password" type="password" value="">
+                    </td>
+                    <td>
+                        <div class="error">%(error_password)s</div>
+                    </td>
+
+                </tr>
+                <tr>
+                    <td>
+                        <label for="verify">Verify Password</label>
+                    </td>
+                    <td>
+                        <input name="verify" type="password" value="">
+                    </td>
+                    <td>
+                        <div class="error">%(error_verify)s</div>
+                    </td>
+                </tr>
+                <tr>
+                    <td>
+                        <label for="email">Email (optional)</label>
+                    </td>
+                    <td>
+                        <input name="email" type="email" value="%(email)s">
+                    </td>
+                    <td>
+                        <div class="error">%(error_email)s</div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+        <input type="submit">
+    </form>
+"""
+
+USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
+def valid_username(username):
+    return username and USER_RE.match(username)
+
+PASS_RE = re.compile(r"^.{3,20}$")
+def valid_password(password):
+    return password and PASS_RE.match(password)
+
+EMAIL_RE = re.compile(r"^[\S]+@[\S]+\.[\S]+$")
+def valid_email(email):
+    return email or EMAIL_RE.match(email)
+
+
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
 
-        header = "<h1>Signup</h1>"
-
-        signup_form = """
-            <form method="post">
-                <table>
-                    <tbody>
-                        <tr>
-                            <td>
-                                <label for="username">Username</label>
-                            </td>
-                            <td>
-                                <input name="username" type="text"/>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label for="password">Password</label>
-                            </td>
-                            <td>
-                                <input name="password" type="password"/>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label for="verify">Verify Password</label>
-                            </td>
-                            <td>
-                                <input name="verify" type="password"/>
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label for="email">Email (optional)</label>
-                            </td>
-                            <td>
-                                <input name="email" type="email"/>
-                            </td>
-                        </tr>
-                    </tbody>
-                </table>
-                <input type="submit"/>
-            </form>
-            """
-
-        main_content = header + signup_form
-        content = page_header + main_content + page_footer
+    def get(self,  username="", email="", error_username="",error_password="",
+            error_verify="", error_email=""):
+        content = page_header + signup_form % {"error_username" : error_username,
+                                                "error_password" : error_password,
+                                                "error_verify" : error_verify,
+                                                "error_email" : error_email,
+                                                "username" : username,
+                                                "email":email}+ page_footer
         self.response.write(content)
+
+    def post(self):
+        error_username = ""
+        error_password = ""
+        error_verify = ""
+        error_email = ""
+
+        have_Error = False
+        username = self.request.get("username")
+        password = self.request.get("password")
+        verify = self.request.get("verify")
+        email = self.request.get("email")
+
+
+        if not valid_username(username):
+            error_username = "That's not a valid username."
+            have_Error = True
+
+        if not valid_password(password):
+            error_password = "That wasn't a valid password"
+            have_Error = True
+        elif password != verify:
+            error_verify = "Your passwords didn't match"
+            have_Error = True
+
+        if email and not valid_email(email):
+            error_email = "That's not a valid email"
+            have_Error = True
+
+        params = {"error_username" : error_username,
+                    "error_password" : error_password,
+                    "error_verify" : error_verify,
+                    "error_email" : error_email,
+                    "username" : username,
+                    "email":email}
+
+
+        if have_Error:
+            content = page_header + signup_form % params + page_footer
+            self.response.write(content)
+        else:
+            self.redirect('/welcome?username=' + username)
 
 class WelcomeHandler(webapp2.RequestHandler):
     def get(self):
-        name = self.request.get(username)
+        name = self.request.get("username")
+
         content = "<strong>Welcome, " + name + "!</strong"
         self.response.write(content)
 
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler),
-    ('/welcomme', WelcomeHandler)
+    ('/welcome', WelcomeHandler)
 ], debug=True)
